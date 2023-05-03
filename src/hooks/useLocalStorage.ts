d00eda32c325befd32ba63ac55a911a4ee8618a2
kladovamaria <mackladova@ya.ru>
2023-05-03 19:06:20 -0400

    shopping cart. No reducers

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+    const [value, setValue] = useState<T>(() => {
+        const jsonVal = localStorage.getItem(key);
+        if (jsonVal != null) return JSON.parse(jsonVal);
+
+        if (typeof initialValue === "function") {
+            return (initialValue as () => T)();
+        } else {
+            return initialValue;
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value])
+
+    return [value, setValue] as [typeof value, typeof setValue];
+
+}
+
+export default useLocalStorage;
